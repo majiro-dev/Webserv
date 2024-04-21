fc717a77503429b286f98a8dbb1727a44e5727e0
Majiro <majiro1996@gmail.com>
2024-04-22 00:07:05 +0200

    relative path fix

---
+++ cgi/path.py
@@ -0,0 +1,16 @@
+import sys
+
+def print_file_content(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print("File not found.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_path>")
+    else:
+        file_path = sys.argv[1]
+        print_file_content(file_path)
